5818ac53d2aba87b8207477a9b091ccf27d861db
Jefferson Esperanza <jefferson.esperanza.03@gmail.com>
2024-01-16 09:32:20 -0600

    Ajustes finales

---
+++ Pagina_2.0/main.js
@@ -0,0 +1,5 @@
+console.log("Conectado");
+
+function redireccionarCDC() {
+    window.location.href = "https://www.cdc.gov/physicalactivity/basics/spanish/los-beneficios-de-la-actividad-fisica.htm#:~:text=Hacer%20actividad%20f%C3%ADsica%20regularmente%20puede,que%20ayudarte%20a%20dormir%20mejor.";
+}
